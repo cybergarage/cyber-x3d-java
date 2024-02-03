c85fcc495e38ebebe4dc8de6f143856de744f4e4
Satoshi Konno <skonno@cybergarage.org>
2024-02-03 15:18:24 +0900

    Import from sourceforge.net

---
+++ examples/x3d2vrml/X3D2VRML.java
@@ -0,0 +1,32 @@
+/******************************************************************
+*
+*	CyberX3D for Java
+*
+*	Copyright (C) Satoshi Konno 1996-2003
+*
+*	File:	x3d2vrml.java
+*
+******************************************************************/
+
+import org.cybergarage.x3d.*;
+
+public class X3D2VRML 
+{
+	public static void main(String args[]) 
+	{
+		int argc = args.length;
+
+		if (argc < 1){
+			System.out.println("Usage: X3D2VRML <filename>");
+			return;
+		}
+	
+		SceneGraph sceneGraph = new SceneGraph();
+	
+		if (sceneGraph.load(args[0]) == true)
+			sceneGraph.print();
+		else
+			System.out.println("\t" + sceneGraph.getParserErrorMessage());
+	}
+}
+
